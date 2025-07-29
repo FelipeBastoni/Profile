eecccd489205799b55797ad36c1e379fb5c28ea2
Felipe Bertão Bastoni <163520381+FelipeBastoni@users.noreply.github.com>
2025-07-29 13:30:55 -0300

    Update jvsc.js

--- jvsc.js
+++ jvsc.js
@@ -3,5 +3,14 @@ function fff(){ window.location.href="https://github.com/FelipeBastoni"; }
 function anim(){
 
     document.getElementById('lg').classList.toggle('ativo');
+    document.getElementById('lg1').classList.toggle('ativo');
+
+}
+
+function modal(){
+
+    const modal = document.getElementById('md');
+    const mdativo = modal.style.display === 'block';
+    modal.style.display = mdativo ? 'none' : 'block';
 
 }
